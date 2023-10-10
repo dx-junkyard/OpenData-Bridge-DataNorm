7cfea1d28d0212ca0c0c7d65c48f5625fc0ec2b1
urashin <s.urakawa@hotmail.co.jp>
2023-10-10 09:22:37 +0900

    feat: コミットし忘れ。ファイルダウンロード機能

---
+++ file_downloader.py
@@ -0,0 +1,45 @@
+import requests
+import os
+import json
+
+def download_file(url, save_path):
+    try:
+        response = requests.get(url, stream=True)
+        response.raise_for_status()
+        with open(save_path, 'wb') as file:
+            for chunk in response.iter_content(chunk_size=8192):
+                file.write(chunk)
+        return True
+    except requests.RequestException:
+        return False
+
+def main():
+    config_file = "download_config.json"
+    download_dir = "./sample_data"
+    download(config_file, download_dir)
+
+def download(config_file, download_dir):
+    # JSONファイルの読み込み
+    with open(config_file, 'r') as f:
+        config = json.load(f)
+
+    # ディレクトリが存在しない場合は作成
+    if not os.path.exists(download_dir):
+        os.makedirs(download_dir)
+
+    for item in config.get('files', []):
+        download_url = item.get('url')
+        default_url = item.get('default_url')
+        filename = item.get('filename')
+
+        save_path = os.path.join(download_dir, filename)
+
+        # ファイルのダウンロード
+        if not download_file(download_url, save_path):
+            print(f"Failed to download from {download_url}. Trying default URL...")
+            if not download_file(default_url, save_path):
+                print(f"Failed to download from default URL {default_url} as well.")
+
+if __name__ == "__main__":
+    main()
+
