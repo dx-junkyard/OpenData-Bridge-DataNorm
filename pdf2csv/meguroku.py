b53d5418d3f7f14a9cad36e38ac48321a2b2ef0b
urashin <s.urakawa@hotmail.co.jp>
2023-12-19 23:01:19 +0900

    refactor: 余計なコード削除、クラスの整理

--- pdf2csv/meguroku.py
+++ pdf2csv/meguroku.py
@@ -5,19 +5,12 @@ import json
 with open('settings.json', 'r', encoding='utf-8') as file:
     settings = json.load(file)
 
-# Define the path to the PDF file
-pdf_path = './R4hirobaichiran.pdf'
 
-# Open the PDF file
-pdf_document = fitz.open(pdf_path)
-
-# 各セクションの情報を格納する辞書を初期化
-section_list = []
 
 # Process each page in the document
 class FacilityOverview:
     def loadPage(self, page):
-        section_data = {}
+        section_list = []
         tables = page.find_tables()
         if tables == None:
             return
@@ -29,6 +22,7 @@ class FacilityOverview:
                 section_info = self.parse_and_set_data(section_def, table.extract()[row_num + 1:row_num + 10])
                 print("section_info = " + str(section_info))
                 section_list.append(section_info)
+        return section_list
 
     def getConfigSection(self,title):
         for section_def in settings["sections"]:
@@ -44,75 +38,41 @@ class FacilityOverview:
         for row in data_rows:
             for item in items:
                 if item in row:
+                    # 項目(item)の次(+1)が項目に対応する値とみなす
                     value = row[row.index(item)+1]
                     section_info[item] = value
-
         return section_info
-#        if section_info:
-#            print("section_info:" + str(section_info))
-#            section_data[title] = section_info
-#        else:
-#            print("section_info:" + str(section_info))
 
+
+class PdfLoader:
+    def loadPDF(self, pdf_path):
+        # Open the PDF file
+        pdf_document = fitz.open(pdf_path)
+        # 各セクションの情報を格納する辞書を初期化
+        page_list = []
+        
+        # Process each page in the document
+        for page_num in range(pdf_document.page_count):
+            page = pdf_document.load_page(page_num)
+            #flist = FacilityList()
+            columns = page.find_tables()[0].extract()[0]
+            if columns[0] == settings["page_title"]:
+                print("find 09")
+                facility_page = FacilityOverview()
+                section_list = facility_page.loadPage(page)
+                page_list.append(section_list)
+        
+        
+        # Close the PDF document
+        pdf_document.close()
+        return page_list
     
-class FacilityList:
-    def getList(self,page_num,page):
-        tables = page.find_tables()
-        if tables == None:
-            return
 
-        print("page n : " + str(page_num))
-        # Process each table found on the page
-        for table_num, table in enumerate(tables):
-            # Convert the table to a pandas DataFrame
-            try:
-                columns = table.extract()[0]
-                data_rows = table.extract()[1:]
-                if len(data_rows) == 0:
-                    continue
-                columns = [col for col in columns if col]
-                if not columns:
-                    continue
-                # Filter out empty strings from data rows
-                data_rows = [[cell for cell in row if cell] for row in data_rows]
-
-                print("columns ->")
-                print(columns)
-                print("data_rows ->")
-                print(data_rows)
-
-                df = pd.DataFrame(data_rows, columns=columns)
-
-                # Remove commas from numeric values
-                df = df.applymap(lambda x: x.replace(',', '') if isinstance(x, str) and x.isdigit() else x)
-
-                # Define the path to the output CSV file
-                csv_path = f'./table_page{page_num}_table{table_num}.csv'
-
-                # Write the DataFrame to a CSV file
-                df.to_csv(csv_path, index=False)
-            except:
-                print("error at " + str(page_num))
-                continue
-
-
-# Process each page in the document
-for page_num in range(pdf_document.page_count):
-    page = pdf_document.load_page(page_num)
-    #flist = FacilityList()
-    columns = page.find_tables()[0].extract()[0]
-#    if columns[0] == "09 保育園\n【用途中分類】":
-    if columns[0] == settings["page_title"]:
-        print("find 09")
-        facility_page = FacilityOverview()
-        facility_page.loadPage(page)
-    #flist.getList(page_num,page)
-
-
-# Close the PDF document
-pdf_document.close()
-
+# Define the path to the PDF file
+pdf_path = './R4hirobaichiran.pdf'
+pdf_loader = PdfLoader()
+page_list = pdf_loader.loadPDF(pdf_path)
 
 # 抽出されたデータを表示
-print("section_list = " + str(section_list))
+print("page_list = " + str(page_list))
 
